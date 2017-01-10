93092db28c5ee12ecfdf2538add96099feb7a40b
Rolo <rolo_lamperouge@foxmail.com>
2017-01-10 23:10:39 +0800

    build test

---
+++ test/tetris/BoardTest.java
@@ -0,0 +1,30 @@
+package tetris;
+
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rolo on 2017/1/10.
+ */
+public class BoardTest {
+    @org.junit.Test
+    public void start() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void squareWidth() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void squareHeight() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void shapeAt() throws Exception {
+
+    }
+
+}
---
+++ test/tetris/ShapeTest.java
@@ -0,0 +1,51 @@
+package tetris;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rolo on 2017/1/10.
+ */
+public class ShapeTest {
+    @Test
+    public void setShape() throws Exception {
+
+    }
+
+    @Test
+    public void setRandomShape() throws Exception {
+
+    }
+
+    @Test
+    public void x() throws Exception {
+
+    }
+
+    @Test
+    public void y() throws Exception {
+
+    }
+
+    @Test
+    public void getShape() throws Exception {
+
+    }
+
+    @Test
+    public void minX() throws Exception {
+
+    }
+
+    @Test
+    public void minY() throws Exception {
+
+    }
+
+    @Test
+    public void rotateRight() throws Exception {
+
+    }
+
+}
